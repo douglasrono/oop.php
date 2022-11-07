5b47c746c6245b0eff6bde87b198ddb639353b71
Douglas Kiprono <69076570+douglasrono@users.noreply.github.com>
2022-11-07 12:36:33 +0300

    Create constants.php

---
+++ constants.php
@@ -0,0 +1,19 @@
+ <?php
+class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+}
+
+echo Goodbye::LEAVING_MESSAGE;
+?> 
+
+ <?php
+class Goodbye {
+  const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+  public function byebye() {
+    echo self::LEAVING_MESSAGE;
+  }
+}
+
+$goodbye = new Goodbye();
+$goodbye->byebye();
+?> 
